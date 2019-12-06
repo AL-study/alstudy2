f9da8418cbc0c747ac604acb014f3822d9b263d0
jungtaeyong <ckck5050@naver.com>
2019-12-06 11:55:15 +0900

    baekjoon 11049 행렬 곱셈 순서+README

---
+++ 다이나믹 프로그래밍/baekjoon 11049 행렬 곱셈 순서.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int n,arr[501][2],dp[501][501];
+int main(){
+	cin>>n;
+	for(int i=1;i<=n;i++){
+		cin>>arr[i][0]>>arr[i][1];
+	}
+	for(int i=1;i<=n-1;i++){
+		dp[i][i+1]=arr[i][0]*arr[i][1]*arr[i+1][1];
+	}
+	for(int l=2;l<=n;l++){
+		for(int i=1;i<=n-l;i++){
+			int j=i+l;
+			for(int k=i;k<j;k++){
+				int temp=dp[i][k]+dp[k+1][j]+arr[i][0]*arr[k][1]*arr[j][1];
+				if(dp[i][j]==0||dp[i][j]>temp){
+					dp[i][j]=temp;
+				}
+			}
+		}
+	}
+	cout<<dp[1][n];
+} 
