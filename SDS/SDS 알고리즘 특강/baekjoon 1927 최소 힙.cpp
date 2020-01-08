f6cf0d051bdd1ab51d417dbce98368a3032a253a
jungtaeyong <ckck5050@naver.com>
2020-01-08 22:06:52 +0900

    baekjoon 1927 최소 힙.cpp(sds 3일차)

---
+++ SDS/SDS 알고리즘 특강/baekjoon 1927 최소 힙.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int n;
+priority_queue<int, vector<int>,greater<int> > pq;
+
+int main(){
+	ios_base :: sync_with_stdio(false);
+cin.tie(NULL);
+cout.tie(NULL);
+	cin>>n;
+	for(int i=0;i<n;i++){
+		int num;
+		cin>>num;
+		if(num!=0){
+			pq.push(num);
+		}else{
+			if(!pq.empty()){
+				cout<<pq.top()<<"\n";
+				pq.pop();
+			}else{
+				cout<<"0\n";
+			}
+		}
+	}
+
+} 
